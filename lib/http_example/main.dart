4185e67b390aa4bdc1465afa34701cce0fc7d452
Gurleen Sethi <sarusethi@rocketmail.com>
2019-08-29 01:13:36 -0400

    [http_example] Add json parsing

--- lib/http_example/main.dart
+++ lib/http_example/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(HttpApp());
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+
+  Post({
+    this.userId,
+    this.id,
+    this.title,
+  });
+
+  factory Post.fromJSON(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+    );
+  }
 }
 
 class HttpApp extends StatelessWidget {
@@ -23,7 +44,11 @@ class _HttpExampleWidgetState extends State<HttpExampleWidget> {
   String _text = "Http Example";
 
   void _fetchPosts() async {
-    final response = await http.get('https://jsonplaceholder.typicode.com/posts');
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/posts');
+    final List<Post> parsedResponse = jsonDecode(response.body)
+        .map<Post>((json) => Post.fromJSON(json))
+        .toList();
     setState(() {
       _text = response.body;
     });
